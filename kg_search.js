f224e437200c0769af0c5ce3531e28d2c7eedb4c
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-03-27 15:26:53 -0700

    Added a way to highlight words.
    Stores strings highlighted and compares them to the word array.

--- kg_search.js
+++ kg_search.js
@@ -26,52 +26,106 @@
 */
 window.onload = init;
 
+var wordCells;
+var wordBackground;
+var string = "";
+
 function init() {
-      document.getElementById("wordSearchTitle").innerHTML = wordSearchTitle;
-      document.getElementById("wordTable").innerHTML = drawWordSearch();
+    document.getElementById("wordSearchTitle").innerHTML = wordSearchTitle;
+    document.getElementById("wordTable").innerHTML = drawWordSearch(letterGrid, wordGrid);
+    document.getElementById("wordList").innerHTML = showList(wordArray);
+
+    setupSearch();
+    document.addEventListener("mouseup", endBackground);
+
+    // Add event listener for solve button 
+    document.getElementById("showSolution").addEventListener("click",
+        function () {
+            // Remove the inline backgroundColor style from each cell 
+            window.alert("THERE IS NO SOLUTION :)");
+            // for (var i = 0; i < wordCells.length; i++) {
+
+            // }
+
+        }
+    );
 }
 
+function setupSearch() {
+    wordCells = document.querySelectorAll("table#wordSearchTable td");
 
+    for (var i = 0; i < wordCells.length; i++) {
+        wordCells[i].onmousedown = setBackground;
+        wordCells[i].style.cursor = "pointer";
+    }
+}
 
+function setBackground(e) {
+    var cursorType;
+    wordBackground = "rgb(101, 101, 101)";
+    cursorType = "pointer";
 
+    e.target.style.backgroundColor = wordBackground;
+    for (var i = 0; i < wordCells.length; i++) {
+        wordCells[i].addEventListener("mouseenter", extendBackground);
+        wordCells[i].style.cursor = cursorType;
+    }
+    e.preventDefault();
+
+}
+
+function extendBackground(e) {
+    e.target.style.backgroundColor = wordBackground;
+    string += e.target.textContent;
+    console.log(string);
+}
+
+function endBackground() {
+    // Remove the event listener for every puzzle cell 
+    for (var i = 0; i < wordCells.length; i++) {
+        wordCells[i].removeEventListener("mouseenter", extendBackground);
+        wordCells[i].style.cursor = "pointer";
+        string = "";
+    }
+}
 
 /*============================================================*/
 
 function drawWordSearch(letters, words) {
-      var rowSize = letters.length;
-      var colSize = letters[0].length;
+    var rowSize = letters.length;
+    var colSize = letters[0].length;
 
-      var htmlCode = "<table id='wordSearchTable'>";
-      htmlCode += "<caption>Word Search</caption>";
+    var htmlCode = "<table id='wordSearchTable'>";
+    htmlCode += "<caption>Word Search</caption>";
 
-      for (var i = 0; i < rowSize; i++) {
-            htmlCode += "<tr>";
+    for (var i = 0; i < rowSize; i++) {
+        htmlCode += "<tr>";
 
-            for (var j = 0; j < colSize; j++) {
-                  if (words[i][j] == " ") {
-                        htmlCode += "<td>";
-                  } else {
-                        htmlCode += "<td class='wordCell'>";
-                  }
-                  htmlCode += letters[i][j];
-                  htmlCode += "</td>";
+        for (var j = 0; j < colSize; j++) {
+            if (words[i][j] == " ") {
+                htmlCode += "<td>";
+            } else {
+                htmlCode += "<td class='wordCell'>";
             }
+            htmlCode += letters[i][j];
+            htmlCode += "</td>";
+        }
 
-            htmlCode += "</tr>";
-      }
-      htmlCode += "</table>";
+        htmlCode += "</tr>";
+    }
+    htmlCode += "</table>";
 
-      return htmlCode;
+    return htmlCode;
 }
 
 function showList(list) {
-      var htmlCode = "<ul id='wordSearchList'>";
+    var htmlCode = "<ul id='wordSearchList'>";
 
-      for (var i = 0; i < list.length; i++) {
-            htmlCode += "<li>" + list[i] + "</li>";
-      }
+    for (var i = 0; i < list.length; i++) {
+        htmlCode += "<li>" + list[i] + "</li>";
+    }
 
-      htmlCode += "</ul>";
+    htmlCode += "</ul>";
 
-      return htmlCode;
+    return htmlCode;
 }
